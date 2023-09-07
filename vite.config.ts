31c357628520e5e5010b117fe05f9157df78e108
Frank Reimann <frank.reimann@jambit.com>
2023-09-07 21:17:54 +0200

    update docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 
 // https://github.com/nonara/ts-patch/issues/106
 // https://github.com/sveltejs/svelte-preprocess/issues/537
+// Note: potentially configure rollup and plugins instead of tsconfig -- https://github.com/jirutka/ts-transformer-export-default-name/tree/master
 // @ts-ignore
 import tspCompiler from 'ts-patch/compiler';
 
