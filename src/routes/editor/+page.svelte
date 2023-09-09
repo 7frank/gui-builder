75f7d43bf653d7e5064372c81bdb861f570e83a2
Frank Reimann <frank.reimann@jambit.com>
2023-09-09 12:26:51 +0200

    format

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,34 +1,32 @@
 <script lang="ts">
-import grapesjs, { Editor, type AddComponentTypeOptions } from 'grapesjs';
-import  'grapesjs/dist/css/grapes.min.css';
+	import grapesjs, { Editor, type AddComponentTypeOptions } from 'grapesjs';
+	import 'grapesjs/dist/css/grapes.min.css';
 
-import plugin from 'grapesjs-preset-webpage';
-import basicBlocks from 'grapesjs-blocks-basic';
-import {onMount} from "svelte"
+	import plugin from 'grapesjs-preset-webpage';
+	import basicBlocks from 'grapesjs-blocks-basic';
+	import { onMount } from 'svelte';
 	import { myNewComponentTypes } from './grapes-test-plugin';
 
-// https://github.com/sveltejs/svelte-preprocess/issues/537
-// this value should be replaced by the typescript compiler in case this is called for this svelte file
-const foo="plugin-test-before"
-const bar="plugin-test-before"
+	// https://github.com/sveltejs/svelte-preprocess/issues/537
+	// this value should be replaced by the typescript compiler in case this is called for this svelte file
+	const foo = 'plugin-test-before';
 
-onMount(()=>{
-	const editor = grapesjs.init({
-  container : '#guiBuilder',
-  // ...
-  plugins: [basicBlocks,myNewComponentTypes,plugin],
-  pluginsOpts: {
-    [plugin]: { /* options */ }
-  }
-  // or
-//   plugins: [
-//     editor => plugin(editor, { /* options */ }),
-//   ],
-});
-
-
-})
-
+	onMount(() => {
+		const editor = grapesjs.init({
+			container: '#guiBuilder',
+			// ...
+			plugins: [basicBlocks, myNewComponentTypes, plugin],
+			pluginsOpts: {
+				[plugin]: {
+					/* options */
+				}
+			}
+			// or
+			//   plugins: [
+			//     editor => plugin(editor, { /* options */ }),
+			//   ],
+		});
+	});
 </script>
 
 <svelte:head>
@@ -36,8 +34,7 @@ onMount(()=>{
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div id="guiBuilder"></div>
+<div id="guiBuilder" />
 
 <style>
-
 </style>
