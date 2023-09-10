c27b1bb488a4ace4f02b02623363e4eef166144d
Frank Reimann <frank.reimann@jambit.com>
2023-09-10 14:58:14 +0200

    try to extract  types via ts-json-schema-generator

--- transformers/svelte-component-transformer.ts
+++ transformers/svelte-component-transformer.ts
@@ -1,25 +1,54 @@
 import type * as ts from 'typescript';
 import type { TransformerExtras, PluginConfig } from 'ts-patch';
 
+import { createGenerator, Settings, createParser } from 'ts-json-schema-generator';
+
 /** Changes string literal 'before' to 'after' */
 export default function transformer(
 	program: ts.Program,
 	pluginConfig: PluginConfig,
-	{ ts: tsInstance }: TransformerExtras
+	{ ts }: TransformerExtras
 ) {
 	return (ctx: ts.TransformationContext) => {
 		const { factory } = ctx;
 
-		return (sourceFile: ts.SourceFile) => {
-			function visit(node: ts.Node): ts.Node {
-				if (tsInstance.isStringLiteral(node) && node.text === 'plugin-test-before') {
-					console.log(sourceFile.fileName, node.kind);
-					return factory.createStringLiteral('after2');
+		function extractExportedVariables(node: ts.Node): ts.Node {
+			if (
+				ts.isVariableStatement(node) &&
+				node.modifiers &&
+				node.modifiers.some((modifier) => modifier.kind === ts.SyntaxKind.ExportKeyword)
+			) {
+				for (const declaration of node.declarationList.declarations) {
+					if (ts.isIdentifier(declaration.name)) {
+						const variableName = declaration.name.text;
+						const typeNode = declaration.type;
+						if (typeNode) {
+							const generator = createGenerator({
+								tsconfig: '../tsconfig.json', // Replace with your tsconfig.json path
+								type: variableName,
+								topRef: true
+							});
+							const jsonSchema = generator.createSchema(typeNode.getText());
+							console.log(
+								`Exported Variable: ${variableName}, JSON Schema: ${JSON.stringify(
+									jsonSchema,
+									null,
+									2
+								)}`
+							);
+						}
+					}
 				}
-
-				return tsInstance.visitEachChild(node, visit, ctx);
 			}
-			return tsInstance.visitNode(sourceFile, visit);
+
+			return ts.visitEachChild(node, extractExportedVariables, ctx);
+		}
+
+		return (sourceFile: ts.SourceFile) => {
+			// we only want to parse svelte files for relevant data
+			if (!sourceFile.fileName.endsWith('.svelte')) return sourceFile;
+			const result = ts.visitNode(sourceFile, extractExportedVariables);
+			return result;
 		};
 	};
 }
