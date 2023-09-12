73108686ccb01fb7b2097f3783b69712814c7978
Frank Reimann <frank.reimann@jambit.com>
2023-09-12 19:26:17 +0200

    fix update logic and properly remove style tags

--- src/routes/editor/sync-css.ts
+++ src/routes/editor/sync-css.ts
@@ -2,26 +2,23 @@
 function copyScriptTagsToIframe(iframe: HTMLIFrameElement, scriptSelector: string): void {
 	const scriptTags = document.querySelectorAll<HTMLScriptElement>(scriptSelector);
 
-	const scriptMap = new Map<string, HTMLScriptElement>(); // Store the last script for each devId
-
 	scriptTags.forEach((scriptTag) => {
 		const devId = scriptTag.getAttribute('data-vite-dev-id');
 		if (!devId) return;
-		// If a script with the same data-vite-dev-id already exists in the map, remove it from the iframe
-		if (scriptMap.has(devId)) {
-			const existingScript = iframe.contentDocument?.querySelector(
-				`script[data-vite-dev-id="${devId}"]`
-			);
-			if (existingScript) {
-				existingScript.remove();
-			}
-		}
-
-		// Add the script to the map
-		scriptMap.set(devId, scriptTag);
 
 		// Add the new script to the iframe
 		iframe.contentDocument?.body.appendChild(scriptTag.cloneNode(true));
+
+		const existingScripts = iframe.contentDocument?.querySelectorAll(
+			`style[data-vite-dev-id="${devId}"]`
+		);
+		if (existingScripts) {
+			const all = [...existingScripts];
+
+			all.pop();
+
+			all.forEach((it) => it.remove());
+		}
 	});
 }
 
@@ -41,8 +38,10 @@ function runCopyScriptEverySecond() {
 /**
  * This function ensures to copy css that svelte generates at runtime , to be copied into our grapesjs iframe
  * Note: this probably will only happen in development mode
+ *
  */
 export function syncCss() {
 	// Run the function every second
-	setInterval(runCopyScriptEverySecond, 1000); // 1000 milliseconds (1 second)
+	runCopyScriptEverySecond();
+	setInterval(runCopyScriptEverySecond, 3000); // 1000 milliseconds (1 second)
 }
