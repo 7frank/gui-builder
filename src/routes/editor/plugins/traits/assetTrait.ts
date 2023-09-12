13c0bbe317f82d624ca7f5986c84aab4021334bc
Frank Reimann <frank.reimann@jambit.com>
2023-09-12 14:16:49 +0200

    replace example

--- src/routes/editor/plugins/traits/assetTrait.ts
+++ src/routes/editor/plugins/traits/assetTrait.ts
@@ -3,13 +3,59 @@ import * as grapesjs from 'grapesjs';
 export const assetTrait: CustomTrait<any> = {
 	name: 'my-asset-select-type',
 	label: 'My Custom Trait',
-	// Define the HTML for the trait
-	templateInput: `
-            <div>
-                <button id="selectAssetBtn">Select Asset</button>
-                <input type="text" id="assetUrl" readonly>
-            </div>
-        `,
+
+	createInput({ trait }: any) {
+		// Here we can decide to use properties from the trait
+		const traitOpts = trait.get('options') || [];
+		const options = traitOpts.length
+			? traitOpts
+			: [
+					{ id: 'url', name: 'URL' },
+					{ id: 'email', name: 'Email' }
+			  ];
+
+		// Create a new element container and add some content
+		const el = document.createElement('div');
+		el.innerHTML = `
+          <select class="href-next__type">
+            ${options.map((opt: any) => `<option value="${opt.id}">${opt.name}</option>`).join('')}
+          </select>
+          <div class="href-next__url-inputs">
+            <input class="href-next__url" placeholder="Insert URL"/>
+          </div>
+          <div class="href-next__email-inputs">
+            <input class="href-next__email" placeholder="Insert email"/>
+            <input class="href-next__email-subject" placeholder="Insert subject"/>
+          </div>
+        `;
+
+		// Let's make our content interactive
+		const inputsUrl = el.querySelector('.href-next__url-inputs') as HTMLDivElement;
+		const inputsEmail = el.querySelector('.href-next__email-inputs') as HTMLDivElement;
+		const inputType = el.querySelector('.href-next__type') as HTMLInputElement;
+		inputType.addEventListener('change', (ev: any) => {
+			switch (ev.target.value) {
+				case 'url':
+					inputsUrl.style.display = '';
+					inputsEmail.style.display = 'none';
+					break;
+				case 'email':
+					inputsUrl.style.display = 'none';
+					inputsEmail.style.display = '';
+					break;
+			}
+		});
+
+		return el;
+	},
+
+	// // Define the HTML for the trait
+	// templateInput: `
+	//         <div>
+	//             <button id="selectAssetBtn">Select Asset</button>
+	//             <input type="text" id="assetUrl" readonly>
+	//         </div>
+	//     `,
 	// Define the events and logic for the button
 	events: {
 		'click #selectAssetBtn': 'openAssetManager'
